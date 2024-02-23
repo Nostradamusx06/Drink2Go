5a68e39ed9470e01d9e158c152886b2a3505aef8
Лев Чернин <cherninleva@gmail.com>
2024-02-23 16:37:05 +0300

    Realization of the range bar and fixes

---
+++ build/scripts/range-slider.js
@@ -0,0 +1,32 @@
+// source/scripts/range-slider.js
+var rangeSlider = document.getElementById("range-slider");
+noUiSlider.create(rangeSlider, {
+  start: [0, 900],
+  connect: true,
+  step: 1,
+  range: {
+    "min": [0],
+    "max": [1e3]
+  }
+});
+if (rangeSlider) {
+  const inputMin = document.getElementById("input-min");
+  const inputMax = document.getElementById("input-max");
+  const inputs = [inputMin, inputMax];
+  rangeSlider.noUiSlider.on("update", function(values, handle) {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+    console.log(arr);
+    rangeSlider.noUiSlider.set(arr);
+  };
+  inputs.forEach((el, index) => {
+    el.addEventListener("change", (e) => {
+      console.log(index);
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+}
+//# sourceMappingURL=range-slider.js.map
