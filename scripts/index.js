617224aef6f4cd423d1dda43b8637a378b688a34
Nostradamusx06 <54951070+Nostradamusx06@users.noreply.github.com>
2024-02-26 23:34:39 +0000

    Deploying to gh-pages from @ Nostradamusx06/Drink2Go@83a3be85d6c267684fe25cda42175db55b581e39 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var a=()=>{let e=document.querySelector(".main-nav"),n=document.querySelector(".main-nav__toggle");e.classList.remove("main-nav--nojs"),n.addEventListener("click",()=>{e.classList.toggle("main-nav--closed"),e.classList.toggle("main-nav--opened")})};a();
